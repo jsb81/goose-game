043545ede05cb115fdd99c4ce1cdfb19710f8f0a
Stefano Ziller <stefano.ziller@infotn.it>
2018-06-01 11:54:56 +0200

    configurazione jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,4 +6,8 @@ node {
     stage ('Unit tests'){
         sh "mvn clean test"
     }
+    stage('Integration test'){
+        echo 'qui dovrei lanciare i test di integrazione'
+        // sh "mvn test.compile failsafe:integration-test"
+    }
 }
